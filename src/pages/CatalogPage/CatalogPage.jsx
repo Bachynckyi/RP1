25661d5d5ea8d9118394006559ac7a5aa0c664ae
Andrew <93881817+Bachynckyi@users.noreply.github.com>
2023-06-05 23:15:05 +0300

    update1.7

--- src/pages/CatalogPage/CatalogPage.jsx
+++ src/pages/CatalogPage/CatalogPage.jsx
@@ -1,15 +1,27 @@
 import { NavLink } from 'react-router-dom';
 import scss from './CatalogPage.module.scss';
 import Footer from "components/Footer/Footer";
+import imageDefault from "../../images/photo_default.jpg";
 
 const CatalogPage = () => {
     return (
       <>
         <div className={scss.container}>
-          <h1>Каталог товарів</h1>
-          <NavLink to="/emali">Емаль</NavLink>
-          <NavLink to="/gruntovki">Грунт</NavLink>
-          <NavLink to="/emali-gruntovki-3v1">Емаль&Грунт 3в1</NavLink>
+          <h1 className={scss.title}>Каталог товарів</h1>
+          <div className={scss.catalog_list}>
+            <NavLink to="/emali" className={scss.catalog_link}>
+              <img className={scss.image_link} src={imageDefault} alt="default"/>
+              <p className={scss.image_title}>Емаль</p>
+            </NavLink>
+            <NavLink to="/gruntovki" className={scss.catalog_link}>
+            <img className={scss.image_link} src={imageDefault} alt="default"/>
+              <p className={scss.image_title}>Грунт</p>
+            </NavLink>
+            <NavLink to="/emali-gruntovki-3v1" className={scss.catalog_link}>
+            <img className={scss.image_link} src={imageDefault} alt="default"/>
+              <p className={scss.image_title}>Грунт&Емаль 3в1</p>
+            </NavLink>
+          </div>
         </div>
         <Footer/>
       </>
